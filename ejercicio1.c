1299d4994d92afc082569d6a3ecf951b975eb6cc
HollowGab <gabri.areani411@gmail.com>
2025-04-03 14:17:15 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,5 +1,29 @@
 //Arellanes Aniceto Gabriel
 //ejercicio1
-#include<stdio.h>
-#include<math.h>
-void main ()
+// Programa en C que calcula n numeros pares y n numeros impares y muestre el ultimo numero par e impar
+#include <stdio.h>
+int main ()
+{
+  int n,m;
+  int par, impar;
+  int i=1;
+  printf ("Cuantos numero quieres calcular evaluar?\n");
+  scanf ("%i",&n);
+  while (i<=n)
+    {
+      if (i%2==0)
+        {
+        par=i;
+      
+      }
+      else 
+      {
+        impar=i;
+      }
+      i++;
+    }
+    printf ("El ultimo numero par es: %i \n",par);
+    printf ("El ultimo numero impar es: %i \n",impar);
+  return 0;
+  // Falta ghacer el do-while
+}
